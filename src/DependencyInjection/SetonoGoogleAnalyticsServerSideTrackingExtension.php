ac66524ae529575c161ee81d156d419cdaa758dc
Joachim Løvgaard <joachim@loevgaard.dk>
2021-03-15 10:38:58 +0100

    Prepend workflow config

--- src/DependencyInjection/SetonoGoogleAnalyticsServerSideTrackingExtension.php
+++ src/DependencyInjection/SetonoGoogleAnalyticsServerSideTrackingExtension.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 namespace Setono\GoogleAnalyticsServerSideTrackingBundle\DependencyInjection;
 
+use Setono\GoogleAnalyticsServerSideTrackingBundle\Workflow\SendHitWorkflow;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
-final class SetonoGoogleAnalyticsServerSideTrackingExtension extends Extension
+final class SetonoGoogleAnalyticsServerSideTrackingExtension extends Extension implements PrependExtensionInterface
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
@@ -21,5 +23,12 @@ final class SetonoGoogleAnalyticsServerSideTrackingExtension extends Extension
 
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.xml');
+    }
+
+    public function prepend(ContainerBuilder $container): void
+    {
+        $container->prependExtensionConfig('framework', [
+            'workflows' => SendHitWorkflow::getConfig(),
+        ]);
     }
 }
--- src/Workflow/SendHitWorkflow.php
+++ src/Workflow/SendHitWorkflow.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Setono\GoogleAnalyticsServerSideTrackingBundle\Workflow;
 
+use Setono\GoogleAnalyticsServerSideTrackingBundle\Entity\HitInterface;
+
 final class SendHitWorkflow
 {
     public const NAME = 'setono_google_analytics_server_side_tracking_send_hit';
@@ -14,5 +16,41 @@ final class SendHitWorkflow
 
     private function __construct()
     {
+    }
+
+    /**
+     * @return array<array-key, string>
+     */
+    public static function getStates(): array
+    {
+        return [
+            HitInterface::STATE_PENDING, HitInterface::STATE_SENT, HitInterface::STATE_FAILED,
+        ];
+    }
+
+    public static function getConfig(): array
+    {
+        return [
+            self::NAME => [
+                'type' => 'state_machine',
+                'marking_store' => [
+                    'type' => 'method',
+                    'property' => 'state',
+                ],
+                'supports' => HitInterface::class,
+                'initial_marking' => HitInterface::STATE_PENDING,
+                'places' => self::getStates(),
+                'transitions' => [
+                    self::TRANSITION_SEND => [
+                        'from' => HitInterface::STATE_PENDING,
+                        'to' => HitInterface::STATE_SENT,
+                    ],
+                    self::TRANSITION_FAIL => [
+                        'from' => HitInterface::STATE_PENDING,
+                        'to' => HitInterface::STATE_FAILED,
+                    ],
+                ],
+            ],
+        ];
     }
 }
