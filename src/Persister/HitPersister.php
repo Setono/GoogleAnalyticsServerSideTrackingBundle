01f936966e2da4b7c4b3aec773ac5674aeb76faa
Joachim Løvgaard <joachim@loevgaard.dk>
2021-08-23 10:55:30 +0200

    Use Setono Doctrine ORM Manager trait lib

--- src/Persister/HitPersister.php
+++ src/Persister/HitPersister.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 namespace Setono\GoogleAnalyticsServerSideTrackingBundle\Persister;
 
 use Doctrine\Persistence\ManagerRegistry;
+use Setono\DoctrineObjectManagerTrait\ORM\ORMManagerTrait;
 use Setono\GoogleAnalyticsMeasurementProtocol\Hit\HitBuilderInterface;
 use Setono\GoogleAnalyticsServerSideTrackingBundle\Entity\Hit;
 use Setono\GoogleAnalyticsServerSideTrackingBundle\Provider\PropertyProviderInterface;
 
 final class HitPersister implements HitPersisterInterface
 {
-    private ManagerRegistry $managerRegistry;
+    use ORMManagerTrait;
 
     private PropertyProviderInterface $propertyProvider;
 
@@ -23,10 +24,7 @@ final class HitPersister implements HitPersisterInterface
 
     public function persistBuilder(HitBuilderInterface $hitBuilder): void
     {
-        $manager = $this->managerRegistry->getManagerForClass(Hit::class);
-        if (null === $manager) {
-            throw new \LogicException(sprintf('No object manager associated with class %s', Hit::class));
-        }
+        $manager = $this->getManager(Hit::class);
 
         foreach ($this->propertyProvider->getProperties() as $property) {
             $manager->persist(Hit::createFromHitBuilder($hitBuilder, $property));
