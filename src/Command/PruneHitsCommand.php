1db16ee4c4eb10c4ad56c7acfd7f5047207b7078
Joachim Løvgaard <joachim@loevgaard.dk>
2021-12-09 12:45:05 +0100

    Rename and add option

--- src/Command/PruneHitsCommand.php
+++ src/Command/PruneHitsCommand.php
@@ -7,11 +7,13 @@ namespace Setono\GoogleAnalyticsServerSideTrackingBundle\Command;
 use Setono\GoogleAnalyticsServerSideTrackingBundle\Repository\HitRepositoryInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Webmozart\Assert\Assert;
 
-final class PruneCommand extends Command
+final class PruneHitsCommand extends Command
 {
-    protected static $defaultName = 'setono:google-analytics:prune';
+    protected static $defaultName = 'setono:google-analytics:prune-hits';
 
     private HitRepositoryInterface $hitRepository;
 
@@ -22,15 +24,24 @@ final class PruneCommand extends Command
      */
     public function __construct(HitRepositoryInterface $hitRepository, int $threshold)
     {
-        parent::__construct();
-
         $this->hitRepository = $hitRepository;
         $this->threshold = $threshold;
+
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this->addOption('threshold', null, InputOption::VALUE_REQUIRED, 'The number of minutes to wait before deleting sent hits', $this->threshold);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $olderThan = (new \DateTimeImmutable())->sub(new \DateInterval(sprintf('PT%dM', $this->threshold)));
+        $threshold = $input->getOption('threshold');
+        Assert::integerish($threshold);
+        Assert::greaterThanEq($threshold, 0);
+
+        $olderThan = (new \DateTimeImmutable())->sub(new \DateInterval(sprintf('PT%dM', (int) $threshold)));
         $this->hitRepository->prune($olderThan);
 
         return 0;
