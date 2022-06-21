c09b3babef0a8057a44828447e88cc4dadc2d7c1
Joachim Løvgaard <joachim@loevgaard.dk>
2022-06-21 09:49:49 +0200

    Use setono/bot-detection-bundle

--- src/Filter/BotFilter.php
+++ src/Filter/BotFilter.php
@@ -4,42 +4,20 @@ declare(strict_types=1);
 
 namespace Setono\GoogleAnalyticsServerSideTrackingBundle\Filter;
 
-use DeviceDetector\Cache\PSR6Bridge;
-use DeviceDetector\Parser\Bot as BotParser;
-use Psr\Cache\CacheItemPoolInterface;
+use Setono\BotDetectionBundle\BotDetector\BotDetectorInterface;
 use Setono\GoogleAnalyticsMeasurementProtocol\Hit\HitBuilderInterface;
-use Symfony\Component\HttpFoundation\RequestStack;
 
 final class BotFilter implements FilterInterface
 {
-    private RequestStack $requestStack;
+    private BotDetectorInterface $botDetector;
 
-    private CacheItemPoolInterface $cache;
-
-    public function __construct(RequestStack $requestStack, CacheItemPoolInterface $cache)
+    public function __construct(BotDetectorInterface $botDetector)
     {
-        $this->requestStack = $requestStack;
-        $this->cache = $cache;
+        $this->botDetector = $botDetector;
     }
 
     public function filter(HitBuilderInterface $hitBuilder): bool
     {
-        $request = $this->requestStack->getMasterRequest();
-        if (null === $request) {
-            return true;
-        }
-
-        $userAgent = $request->headers->get('user-agent');
-        if (null === $userAgent) {
-            return true;
-        }
-
-        $botParser = new BotParser();
-        $botParser->setUserAgent($userAgent);
-        $botParser->discardDetails();
-        $botParser->setCache(new PSR6Bridge($this->cache));
-        $result = $botParser->parse();
-
-        return null === $result;
+        return !$this->botDetector->isBotRequest();
     }
 }
--- tests/Filter/BotFilterTest.php
+++ tests/Filter/BotFilterTest.php
@@ -5,30 +5,28 @@ declare(strict_types=1);
 namespace Setono\GoogleAnalyticsServerSideTrackingBundle\Tests\Filter;
 
 use PHPUnit\Framework\TestCase;
+use Prophecy\PhpUnit\ProphecyTrait;
+use Setono\BotDetectionBundle\BotDetector\BotDetectorInterface;
 use Setono\GoogleAnalyticsMeasurementProtocol\Hit\HitBuilder;
 use Setono\GoogleAnalyticsMeasurementProtocol\Hit\HitBuilderInterface;
 use Setono\GoogleAnalyticsServerSideTrackingBundle\Filter\BotFilter;
-use Symfony\Component\Cache\Adapter\NullAdapter;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * @covers \Setono\GoogleAnalyticsServerSideTrackingBundle\Filter\BotFilter
  */
 final class BotFilterTest extends TestCase
 {
+    use ProphecyTrait;
+
     /**
      * @test
      */
     public function it_filters(): void
     {
-        $request = Request::create('/', 'GET', [], [], [], [
-            'HTTP_USER_AGENT' => 'Googlebot',
-        ]);
-        $requestStack = new RequestStack();
-        $requestStack->push($request);
+        $botDetector = $this->prophesize(BotDetectorInterface::class);
+        $botDetector->isBotRequest(null)->willReturn(true);
 
-        $filter = new BotFilter($requestStack, new NullAdapter());
+        $filter = new BotFilter($botDetector->reveal());
 
         $hitBuilder = new HitBuilder(HitBuilderInterface::HIT_TYPE_PAGEVIEW);
 
