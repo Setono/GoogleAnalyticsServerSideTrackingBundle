5e5b28941f947925b292412d7fc2fb4d6c307519
Joachim Løvgaard <joachim@loevgaard.dk>
2022-06-21 10:58:19 +0200

    Try to remove client id bundle

--- composer-unused.php
+++ composer-unused.php
@@ -9,7 +9,6 @@ return static function (Configuration $config): Configuration {
     return $config
         ->addNamedFilter(NamedFilter::fromString('gedmo/doctrine-extensions'))
         ->addNamedFilter(NamedFilter::fromString('nyholm/psr7'))
-        ->addNamedFilter(NamedFilter::fromString('setono/client-id-bundle'))
         ->addNamedFilter(NamedFilter::fromString('setono/consent-bundle'))
         ->addNamedFilter(NamedFilter::fromString('symfony/http-client'))
         ->addNamedFilter(NamedFilter::fromString('symfony/lock'))
