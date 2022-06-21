574e60c4bbe8ca601a461ec06dcf85b5c2347c7b
Joachim Løvgaard <joachim@loevgaard.dk>
2022-06-21 11:13:56 +0200

    Mock kernel instead

--- tests/EventListener/PersistHitBuildersSubscriberTest.php
+++ tests/EventListener/PersistHitBuildersSubscriberTest.php
@@ -5,26 +5,24 @@ declare(strict_types=1);
 namespace Setono\GoogleAnalyticsServerSideTrackingBundle\Tests\EventListener;
 
 use PHPUnit\Framework\TestCase;
+use Prophecy\PhpUnit\ProphecyTrait;
 use Setono\GoogleAnalyticsMeasurementProtocol\Hit\HitBuilder;
 use Setono\GoogleAnalyticsMeasurementProtocol\Hit\HitBuilderInterface;
 use Setono\GoogleAnalyticsMeasurementProtocol\Hit\HitBuilderStack;
 use Setono\GoogleAnalyticsServerSideTrackingBundle\EventListener\PersistHitBuildersSubscriber;
 use Setono\GoogleAnalyticsServerSideTrackingBundle\Persister\HitPersisterInterface;
-use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
-use Symfony\Component\Config\Loader\LoaderInterface;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
-use Symfony\Component\HttpKernel\Kernel;
-use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 /**
  * @covers \Setono\GoogleAnalyticsServerSideTrackingBundle\EventListener\PersistHitBuildersSubscriber
  */
 final class PersistHitBuildersSubscriberTest extends TestCase
 {
+    use ProphecyTrait;
+
     /**
      * @test
      */
@@ -39,30 +37,9 @@ final class PersistHitBuildersSubscriberTest extends TestCase
             }
         };
 
-        /** @psalm-suppress PropertyNotSetInConstructor */
-        $kernel = new class() extends Kernel {
-            use MicroKernelTrait;
+        $kernel = $this->prophesize(HttpKernelInterface::class);
 
-            public function __construct()
-            {
-                parent::__construct('test', true);
-            }
-
-            public function registerBundles()
-            {
-                return [];
-            }
-
-            protected function configureRoutes(RoutingConfigurator $routingConfigurator)
-            {
-            }
-
-            protected function configureContainer(ContainerBuilder $c, LoaderInterface $loader)
-            {
-            }
-        };
-
-        $event = new ResponseEvent($kernel, new Request(), HttpKernelInterface::MASTER_REQUEST, new Response());
+        $event = new ResponseEvent($kernel->reveal(), new Request(), HttpKernelInterface::MASTER_REQUEST, new Response());
 
         $hitBuilderStack = new HitBuilderStack();
         $hitBuilderStack->push(new HitBuilder(HitBuilderInterface::HIT_TYPE_PAGEVIEW));
